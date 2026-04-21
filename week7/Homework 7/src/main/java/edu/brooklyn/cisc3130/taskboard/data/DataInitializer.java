08b284cf856ef9d5f5ab73b72dbb5fc1b36a143d
jerryzhang017 <jerryzhang017@gmail.com>
2026-04-21 11:31:01 -0400

    missing parameter

--- week7/Homework 7/src/main/java/edu/brooklyn/cisc3130/taskboard/data/DataInitializer.java
+++ week7/Homework 7/src/main/java/edu/brooklyn/cisc3130/taskboard/data/DataInitializer.java
@@ -26,47 +26,47 @@ public class DataInitializer implements CommandLineRunner {
             taskRepository.save(new Task(
                 null, "Study for Midterm", 
                 "Review chapters 1-5", 
-                false, Task.Priority.HIGH, null, null));
+                false, Task.Priority.HIGH, null, null, false));
             
             taskRepository.save(new Task(
                 null, "Buy groceries", 
                 "Milk, eggs, bread", 
-                true, Task.Priority.LOW, null, null));
+                true, Task.Priority.LOW, null, null, false));
 
             taskRepository.save(new Task(
                 null, "Complete Homework 5",
                 "Finish Spring REST API assignment",
-                true, Task.Priority.HIGH, null, null));
+                true, Task.Priority.HIGH, null, null, false));
 
             taskRepository.save(new Task(
                 null, "Read Chapter 6",
                 "Read database chapter in textbook",
-                false, Task.Priority.MEDIUM, null, null));
+                false, Task.Priority.MEDIUM, null, null, false));
 
             taskRepository.save(new Task(
                 null, "Submit Lab Report",
                 "Upload lab report to Blackboard",
-                false, Task.Priority.HIGH, null, null));
+                false, Task.Priority.HIGH, null, null, false));
 
             taskRepository.save(new Task(
                 null, "Clean room",
                 "Vacuum and organize desk",
-                true, Task.Priority.LOW, null, null));
+                true, Task.Priority.LOW, null, null, false));
 
             taskRepository.save(new Task(
                 null, "Exercise",
                 "Go to the gym",
-                false, Task.Priority.MEDIUM, null, null));
+                false, Task.Priority.MEDIUM, null, null, false));
 
             taskRepository.save(new Task(
                 null, "Call mom",
                 "Catch up with family",
-                true, Task.Priority.LOW, null, null));
+                true, Task.Priority.LOW, null, null, false));
 
             taskRepository.save(new Task(
                 null, "Review lecture notes",
                 "Go over notes from last class",
-                false, Task.Priority.MEDIUM, null, null));
+                false, Task.Priority.MEDIUM, null, null, false));
         }
     }
 }
